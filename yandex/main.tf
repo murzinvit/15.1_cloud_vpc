41ede7ad33633824fe62cefeab15401a5ccc7cd8
Vitalik <murzinvit@gmail.com>
2021-12-21 01:51:43 -0500

    add conf

---
+++ yandex/main.tf
@@ -0,0 +1,79 @@
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = var.vm_name
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.nat_image
+    }
+  }
+
+  network_interface {
+    subnet_id  = yandex_vpc_subnet.subnet-1.id
+    nat        = true
+    ip_address = var.internal_nat_ip
+  }
+
+  metadata = {
+    user-data = "${file("meta.txt")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name = var.vm2_name
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.vm2_image
+    }
+  }
+
+  network_interface {
+    subnet_id  = yandex_vpc_subnet.subnet-1.id
+    ip_address = var.vm2_ip
+  }
+
+  metadata = {
+    user-data = "${file("meta.txt")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network-1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = var.subnet_name
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+
---
+++ yandex/variables.tf
@@ -0,0 +1,42 @@
+variable "nat_image" {
+  default = "fd80mrhj8fl2oe87o4e1"
+}
+
+variable "vm_name" {
+  default = "nat-1"
+}
+
+variable "internal_nat_ip" {
+  default = "192.168.10.254"
+}
+
+variable "subnet_name" {
+  default = "public"
+}
+
+variable "token" {
+  default = ""
+}
+
+variable "cloud_id" {
+  default = ""
+}
+
+variable "folder_id" {
+  default = "b1g3k25am6h6fb9b04uo"
+}
+
+variable "zone" {
+  default = "ru-central1-a"
+}
+variable "vm2_name" {
+  default = "station-1"
+}
+
+variable "vm2_ip" {
+  default = "192.168.10.10"
+}
+
+variable "vm2_image" {
+  default = "fd80mrhj8fl2oe87o4e1"
+}
